6f68177d0fe408ab553591740a68eaca64171120
Zak Blystone <zakblystone@gmail.com>
2020-02-16 01:26:09 -0800

    - Boolean literal renders as a filled box now instead of 'X' - Enum literals render nicer - String literals wrap text

--- lua/blueprints/graph/pintypes/bool.lua
+++ lua/blueprints/graph/pintypes/bool.lua
@@ -2,6 +2,29 @@ AddCSLuaFile()
 
 local PIN = {}
 
+local surface = surface or {}
+local surface_setDrawColor = surface.SetDrawColor
+local surface_drawRect = surface.DrawRect
+
+function PIN:GetLiteralSize()
+
+	return  25, 25
+
+end
+
+function PIN:DrawLiteral(x,y,w,h,alpha)
+
+	surface_setDrawColor( 50,50,50,150*alpha )
+	surface_drawRect(x,y,w,h)
+
+	if self:GetLiteral() == "true" then
+		--surface_setMaterial( checked )
+		surface_setDrawColor( 255,255,255,255*alpha )
+		surface_drawRect(x+4,y+4,w-8,h-8)
+	end
+
+end
+
 function PIN:OnClicked()
 
 	local value = self:GetLiteral()
--- lua/blueprints/graph/pintypes/string.lua
+++ lua/blueprints/graph/pintypes/string.lua
@@ -1,6 +1,47 @@
 AddCSLuaFile()
 
 local PIN = {}
+local font = "NodeLiteralFont"
+
+local surface = surface or {}
+local surface_setDrawColor = surface.SetDrawColor
+local surface_drawRect = surface.DrawRect
+
+local paddingH = 10
+local paddingV = 0
+
+function PIN:Setup()
+
+	if CLIENT then
+		self.wrap = bptextwrap.New():SetFont(font):SetMaxWidth(400):SetText("TEXT")
+		self:OnLiteralChanged( nil, self:GetLiteral() )
+	end
+
+end
+
+function PIN:OnLiteralChanged( old, new )
+
+	if new then
+		self.wrap:SetText(new)
+	end
+
+end
+
+function PIN:GetLiteralSize()
+
+	local w,h = self.wrap:GetSize()
+	return math.max(w + paddingH, 30), h + paddingV
+
+end
+
+function PIN:DrawLiteral(x,y,w,h,alpha)
+
+	surface_setDrawColor( 50,50,50,150*alpha )
+	surface_drawRect(x,y,w,h)
+
+	self.wrap:Draw(x+paddingH*.5,y+paddingV*.5,255,255,255,255 * alpha)
+
+end
 
 function PIN:OnClicked()
 
