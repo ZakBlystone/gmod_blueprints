6ac944269ab02e3f422d17b81598d89c3d8bed44
Zak Blystone <zakblystone@gmail.com>
2020-04-20 01:12:47 -0700

    Added non-relevant weak object test

--- lua/blueprints/tests/weak.lua
+++ lua/blueprints/tests/weak.lua
@@ -66,3 +66,25 @@ function LINKER_TEST_POST()
 	ASSERT(w() == rpt)
 
 end
+
+function LINKER_TEST_NON_RELEVANT()
+
+	local stream = bpstream.New("test", bpstream.MODE_String):Out()
+	local pt = bppintype.New(bpschema.PN_Ref,nil,"Entity")
+	local pt2 = bppintype.New(bpschema.PN_Ref,nil,"Entity")
+	local pt3 = bppintype.New(bpschema.PN_Ref,nil,"Other")
+	local w = bpcommon.Weak(pt3)
+	stream:Object( pt )
+	stream:Object( pt2 )
+	stream:Object( w )
+	local data = stream:Finish()
+
+	stream = bpstream.New("test2", bpstream.MODE_String, data):In()
+	local rpt = stream:Object()
+	local rpt2 = stream:Object()
+	local w = stream:Object()
+
+	ASSERT(w ~= nil)
+	ASSERT(w() == nil)
+
+end
