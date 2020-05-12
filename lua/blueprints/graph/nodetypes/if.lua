7d7d56d1531ea90f6c9267e2b3aa13cca83963e1
Zak Blystone <zakblystone@gmail.com>
2020-05-11 21:12:50 -0700

    CheckValid node compiles like If node

--- lua/blueprints/graph/nodetypes/if.lua
+++ lua/blueprints/graph/nodetypes/if.lua
@@ -41,9 +41,16 @@ function NODE:GeneratePins(pins)
 		bpschema.PN_Exec
 	)
 
-	pins[#pins+1] = dataPin
+end
+
+function NODE:GetConditionCode(compiler)
+
+	return compiler:GetPinCode( self:FindPin(PD_In, "Condition") )
 
 end
+
+function NODE:GetFalsePin() return self:FindPin(PD_Out, "False") end
+function NODE:GetTruePin() return self:FindPin(PD_Out, "True") end
 
 function NODE:Compile(compiler, pass)
 
@@ -55,12 +62,12 @@ function NODE:Compile(compiler, pass)
 
 	elseif pass == CP_MAINPASS then
 
-		local pinFalse = self:FindPin(PD_Out, "False")
-		local pinTrue = self:FindPin(PD_Out, "True")
+		local pinFalse = self:GetFalsePin()
+		local pinTrue = self:GetTruePin()
 
 		local falseConnections = pinFalse:GetConnectedPins()
 		local trueConnections = pinTrue:GetConnectedPins()
-		local cond = compiler:GetPinCode( self:FindPin(PD_In, "Condition") )
+		local cond = self:GetConditionCode(compiler)
 
 		--if self.__nextExec then print( self:ToString() .. " NEXT: " .. self.__nextExec:ToString() ) else print( self:ToString() .. " NO NEXT") end
 
